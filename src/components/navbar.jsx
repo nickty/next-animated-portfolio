0fb01c70d503c4a24e02d75cd9b813b4a91f0866
Mizanur Rahman <mizan@arogga.net>
2024-02-06 15:59:25 +0600

    header 90 percent done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -14,7 +15,14 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      <div>
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          <Link href={link.url} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+      <div className="md:hidden lg:flex justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -25,7 +33,52 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
-      <div>
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="#">
+          <Image src="/github.png" alt="mizan github" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image
+            src="/dribbble.png"
+            alt="mizan dribbble"
+            width={24}
+            height={24}
+          />
+        </Link>
+        <Link href="#">
+          <Image
+            src="/instagram.png"
+            alt="mizan instagram"
+            width={24}
+            height={24}
+          />
+        </Link>
+        <Link href="#">
+          <Image
+            src="/facebook.png"
+            alt="mizan facebook"
+            width={24}
+            height={24}
+          />
+        </Link>
+        <Link href="#">
+          <Image
+            src="/pinterest.png"
+            alt="mizan pinterest"
+            width={24}
+            height={24}
+          />
+        </Link>
+        <Link href="#">
+          <Image
+            src="/linkedin.png"
+            alt="mizan linkedin"
+            width={24}
+            height={24}
+          />
+        </Link>
+      </div>
+      <div className="md:hidden">
         <button
           className="w-10 h-8 flex flex-col justify-between z-50 relative"
           onClick={() => setOpen(!open)}
