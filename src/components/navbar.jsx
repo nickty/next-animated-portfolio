2fcb5550798e53f7b23632a5a39e25d9e8357d9e
Mizanur Rahman <mizan@arogga.net>
2024-02-06 15:38:09 +0600

    on click menu show and hide

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       <div>
@@ -14,7 +25,25 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
-      <div>1</div>
+      <div>
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+        </button>
+        {open && (
+          <div className="absolute bg-black top-0 left-0 w-screen h-screen text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
